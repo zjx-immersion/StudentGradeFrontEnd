cf3b9b5065a6eccb4de3d89b63b563f3c6ca5c44
zjx-immersion <jianxin_home@163.com>
2017-08-03 23:46:31 +0800

    [Jason]added the add student form submit and validate logic

--- js/add_student.js
+++ js/add_student.js
@@ -1,59 +1,53 @@
 $(function() {
   function submit() {
-    let formData = $(this).serializeArray();
+    let form = $("#studentform");
+    let formData = form.serializeArray();
     console.log("Original Data: " );
     console.log(formData);
     localStorage.setItem("form", JSON.stringify(formData));
     console.log("Storage Data: " );
     console.log(JSON.parse(localStorage.getItem("form")));
-    location.href="result.html";
+    $("#successLabel").fadeToggle(3000, function(){
+      $(this).hide();
+      form.trigger("reset");
+    });
+
+    // $("#success-label").fadeOut();
     event.preventDefault();
   };
 
-  $("#examform").validate({
-    showErrors: function(errorMap, errorList) {
-        // Do nothing here
-        console.log(errorList);
-        let errorStr = errorList.map(err => "<p>"+err.message+"</p>").join("");
-        $(".error").html(errorStr);
-        window.location.hash = '.error';
-    },
-    focusCleanup: true,
-    focusInvalid: false,
-   // Specify validation rules
+  $("#studentform").validate({
+
    rules: {
      // The key name on the left side is the name attribute
      // of an input field. Validation rules are defined
      // on the right side
-     classes: "required",
-     number: "required",
-     name: {
+     name: "required",
+     age: "required",
+     class: "required",
+     phone: "required",
+     email: {
        required: true,
        // Specify that email should be validated
        // by the built-in "email" rule
        email: true
-     },
-     password: {
-       required: true,
-       minlength: 5
      }
    },
    // Specify validation error messages
    messages: {
-     classes: "Please enter your class",
-     number: "Please enter your number",
+     name: "Please enter your name",
+     age: "Please enter your age",
+     class: "Please enter your number",
+     phone: "Please enter your phone",
      password: {
        required: "Please provide a password",
        minlength: "Your password must be at least 5 characters long"
-     },
-     name: "Please enter a valid email address"
+     }
    },
    // Make sure the form is submitted to the destination defined
    // in the "action" attribute of the form when valid
    submitHandler: function(form) {
-     console.log("1----------------");
      submit();
-     console.log("2----------------");
    }
  });
 })
